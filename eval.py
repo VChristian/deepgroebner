01c56fab01777afa1beda10bffcf0cb1809eef9c
Christian Varner <christianvarner@Christians-MacBook-Pro.local>
2020-12-28 20:38:49 -0500

    Added pnet to eval.py script

--- eval.py
+++ eval.py
@@ -12,7 +12,7 @@ import gym
 
 from deepgroebner.buchberger import LeadMonomialsEnv
 from deepgroebner.pg import PGAgent
-from deepgroebner.networks import MultilayerPerceptron, ParallelMultilayerPerceptron, AttentionPMLP, TransformerPMLP
+from deepgroebner.networks import MultilayerPerceptron, ParallelMultilayerPerceptron, AttentionPMLP, TransformerPMLP, PointerNetwork
 from deepgroebner.wrapped import CLeadMonomialsEnv
 
 
@@ -56,7 +56,7 @@ def make_parser():
 
     policy = parser.add_argument_group('policy model')
     policy.add_argument('--policy_model',
-                        choices=['mlp', 'pmlp', 'apmlp', 'tpmlp'],
+                        choices=['mlp', 'pmlp', 'apmlp', 'tpmlp', 'pnet'],
                         default='pmlp',
                         help='policy network type')
     policy.add_argument('--policy_kwargs',
@@ -127,8 +127,10 @@ def make_policy_network(args):
             policy_network = ParallelMultilayerPerceptron(**args.policy_kwargs)
         elif args.policy_model == 'apmlp':
             policy_network = AttentionPMLP(**args.policy_kwargs)
-        else:
+        elif args.policy_model == 'tpmlp':
             policy_network = TransformerPMLP(**args.policy_kwargs)
+        else:
+            policy_network = PointerNetwork(**args.policy_kwargs)
         batch = np.zeros((1, 10, 2 * args.k * int(args.distribution.split('-')[0])), dtype=np.int32)
     policy_network(batch)  # build network
     if args.policy_weights != "":
